1d48a99dceba20c359de39455001e9d7c2acc064
JoeSz <ujbeszel@gmail.com>
2019-10-16 22:20:11 +0200

    Check plugin updates, add some notes and constants

--- exopite-frontend-notifications/exopite-frontend-notifications.php
+++ exopite-frontend-notifications/exopite-frontend-notifications.php
@@ -31,11 +31,25 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
+ * ToDos:
+ * - AJAX to check notifications
+ *   - check je 1 - 5 min?
+ * - Filter to add nofitications (2 filter)
+ *   - first is for "standard" notificaitons, from other plugins or the themes
+ *   - second for AJAX
+ * - DB? CPT?
+ * - Notification to "dismiss", dismiss is by user
+ * - Notifications for roles and users
+ */
+
+/**
  * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'EXOPITE_FRONTEND_NOTIFICATIONS_VERSION', '1.0.0' );
+define( 'EXOPITE_FRONTEND_NOTIFICATIONS_VERSION', '20190705' );
+define( 'EXOPITE_FRONTEND_NOTIFICATIONS_PLUGIN_NAME', 'exopite-frontend-notifications' );
+define( 'EXOPITE_FRONTEND_NOTIFICATIONS_PATH', plugin_dir_path( __FILE__ ) );
 
 /**
  * The code that runs during plugin activation.
@@ -57,6 +71,56 @@ function deactivate_exopite_frontend_notifications() {
 
 register_activation_hook( __FILE__, 'activate_exopite_frontend_notifications' );
 register_deactivation_hook( __FILE__, 'deactivate_exopite_frontend_notifications' );
+
+ /**
+ * Update
+ */
+if ( is_admin() ) {
+
+    /**
+     * A custom update checker for WordPress plugins.
+     *
+     * Useful if you don't want to host your project
+     * in the official WP repository, but would still like it to support automatic updates.
+     * Despite the name, it also works with themes.
+     *
+     * @link http://w-shadow.com/blog/2011/06/02/automatic-updates-for-commercial-themes/
+     * @link https://github.com/YahnisElsts/plugin-update-checker
+     * @link https://github.com/YahnisElsts/wp-update-server
+     */
+    if( ! class_exists( 'Puc_v4_Factory' ) ) {
+
+        require_once join( DIRECTORY_SEPARATOR, array( EXOPITE_FRONTEND_NOTIFICATIONS_PATH, 'vendor', 'plugin-update-checker', 'plugin-update-checker.php' ) );
+
+    }
+
+    $MyUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+        'https://update.yourwebsite.ext/?action=get_metadata&slug=' . EXOPITE_FRONTEND_NOTIFICATIONS_PLUGIN_NAME, //Metadata URL.
+        __FILE__, //Full path to the main plugin file.
+        EXOPITE_FRONTEND_NOTIFICATIONS_PLUGIN_NAME //Plugin slug. Usually it's the same as the name of the directory.
+    );
+
+    /**
+     * Add plugin upgrade notification
+     * https://andidittrich.de/2015/05/howto-upgrade-notice-for-wordpress-plugins.html
+     *
+     * This version add an extra <p> after the notice.
+     * I want that to remove later.
+     */
+    add_action('in_plugin_update_message-plugin-name/plugin-name.php', 'show_upgrade_notification', 10, 2);
+    function show_upgrade_notification( $current_plugin_metadata, $new_plugin_metadata ){
+       // check "upgrade_notice"
+       if (isset( $new_plugin_metadata->upgrade_notice ) && strlen( trim( $new_plugin_metadata->upgrade_notice ) )  > 0 ) {
+
+            echo '<span style="background-color:#d54e21;padding:6px;color:#f9f9f9;margin-top:10px;display:block;"><strong>' . esc_html( 'Upgrade Notice', 'plugin-name' ) . ':</strong><br>';
+            echo esc_html( $new_plugin_metadata->upgrade_notice );
+            echo '</span>';
+
+       }
+    }
+
+}
+// End Update
 
 /**
  * The core plugin class that is used to define internationalization,
