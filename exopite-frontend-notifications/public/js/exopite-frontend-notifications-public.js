e6b93f819b2a6078a94bb88cd2d60cdb366a328e
JoeSz <ujbeszel@gmail.com>
2019-10-16 22:22:45 +0200

    Implement notifications

--- exopite-frontend-notifications/public/js/exopite-frontend-notifications-public.js
+++ exopite-frontend-notifications/public/js/exopite-frontend-notifications-public.js
@@ -1,42 +1,140 @@
+const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key, value) => {
+        if (typeof value === "object" && value !== null) {
+            if (seen.has(value)) {
+                return;
+            }
+            seen.add(value);
+        }
+        return value;
+    };
+};
+
 (function( $ ) {
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	// https://learn.jquery.com/code-organization/concepts/
+	var exopiteFrontendNotifications = {
+		init: function( settings ) {
+            exopiteFrontendNotifications.displayMessages( efn_messages );
+            if (efn_settings.ajax) {
+                setTimeout(exopiteFrontendNotifications.checkAJAX, efn_settings.interval);
+            }
 
-	$(function() {
+        },
+        checkAJAX: function () {
 
-		Lobibox.notify('default', {
-			pauseDelayOnHover: true,
-			continueDelayOnInactiveTab: false,
-			msg: 'Lorem ipsum dolor sit amet hears farmer indemnity inherent.'
-		});
+            $.ajax({
+                cache: false,
+                type: "POST",
+                url: efn_settings.ajaxurl,
+                data: {
+                    'action': 'efn_get_notifications',
+                },
+                success: function( response ){
 
-	});
+                    // console.log( 'response: ' + JSON.stringify( response ) );
+                    var messages = JSON.parse(response);
+                    exopiteFrontendNotifications.displayMessages(messages);
+
+                },
+                error: function( xhr, status, error ) {
+                    console.log( 'Status: ' + xhr.status );
+                    console.log( 'Error: ' + xhr.responseText );
+                },
+                complete: function() {
+                    // Schedule the next request when the current one's complete
+                    setTimeout(exopiteFrontendNotifications.checkAJAX, efn_settings.interval );
+                }
+            });
+
+        },
+        displayMessages: function ( messages ) {
+			var thisClass = this;
+
+            $.each(messages, function (k, message) {
+
+                var args = {
+                    pauseDelayOnHover: message.pauseDelayOnHover,
+                    continueDelayOnInactiveTab: message.continueDelayOnInactiveTab,
+                    delay: message.delay,
+                    closable: message.closable,
+                    closeOnClick: message.closeOnClick,
+                    width: message.width,
+                    messageHeight: message.messageHeight,
+                    sound: message.sound,
+                    position: message.position,
+                    onClickUrl: message.onClickUrl,
+                    showAfterPrevious: message.showAfterPrevious,
+                    position: message.position,
+                    img: message.img,
+                    title: message.title,
+                    msg: message.msg,
+                    showClass: message.showClass,
+                    hideClass: message.hideClass,
+                    icon: message.icon,
+                    iconSource: message.iconSource,
+                    size: message.size,
+                    // onClick: function(e){
+                    //     console.log('e: ' + JSON.stringify(e, getCircularReplacer()));
+                    //     console.log('me: ' + JSON.stringify(this, getCircularReplacer()));
+                    //     console.log('el: ' + JSON.stringify(this.$el.attr('class'), getCircularReplacer()));
+                    // },
+                    // onHide: function(){
+                    //     console.log('me: ' + JSON.stringify(this, getCircularReplacer()));
+                    //     console.log('el: ' + JSON.stringify(this.$el.attr('class'), getCircularReplacer()));
+                    // },
+                    // onAutoDismiss: function (){
+                    //     console.log('me: ' + JSON.stringify(this, getCircularReplacer()));
+                    //     console.log('el: ' + JSON.stringify(this.$el.attr('class'), getCircularReplacer()));
+                    // },
+                    onDismiss: function (e){
+                        if ( ! efn_settings.ajax ) return;
+                        try {
+                            if (typeof (this.$options) !== "undefined" && typeof this.$options.callback !== "undefined" && this.$options.callback !== null) {
+
+                                $.ajax({
+                                    cache: false,
+                                    type: "POST",
+                                    url: efn_settings.ajaxurl,
+                                    data: {
+                                        'action': this.$options.callback,
+                                        'element': this.$options,
+                                    },
+                                    success: function( response ){
+
+                                        console.log( 'response: ' + JSON.stringify( response ) );
+
+                                    },
+                                    error: function( xhr, status, error ) {
+                                        console.log( 'Status: ' + xhr.status );
+                                        console.log( 'Error: ' + xhr.responseText );
+                                    },
+                                });
+
+
+                            }
+                        } catch(error) {}
+                    },
+
+                }
+
+                if (typeof message.id !== "undefined" && message.id !== null) {
+                    args.id = message.id;
+                }
+
+                if (typeof message.callback !== "undefined" && message.callback !== null) {
+                    args.callback = message.callback;
+                }
+
+                Lobibox.notify(message.type, args);
+
+			});
+
+		},
+	};
+
+	$( document ).ready( exopiteFrontendNotifications.init );
 
 })( jQuery );
